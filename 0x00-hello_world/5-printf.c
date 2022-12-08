eb87f825ad1a5d615f389ffea285a01421e397c8
festusmaithyakcau <festusmaithya264@gmail.com>
2022-12-08 03:11:33 -0800

    fixed a MINOR FIX

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/*
+ *main - to print the specified string
+ *Return : 0 to exit
+*/
 int main(void)
 
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+	return (0);
 }
