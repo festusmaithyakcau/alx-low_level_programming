16bc4606271b8420652f78981dfb564c5a49b8d5
festusmaithyakcau <festusmaithya264@gmail.com>
2022-12-10 06:09:49 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,16 @@
 
 #include <time.h>
 
-/* more headers goes there */
+#include <stdio.h>
 
-
-
-/* betty style doc for function main goes there */
+/*
+ * main -Entry point
+ *
+ * Description:print value of n and the status ; zero ,positive, negative
+ *
+ * Return : Always 0 (Success)
+ *
+*/
 
 int main(void)
 
@@ -14,13 +19,24 @@ int main(void)
 
 		int n;
 
-
-
 		srand(time(0));
 
 		n = rand() - RAND_MAX / 2;
 
-		/* your code goes there */
+
+       		/*my final code*/
+
+		if (n > 0)
+
+		printf("%i is positive\n", n);
+
+		else if (n == 0)
+
+		printf("%i is zero\n", n);
+
+		else
+
+		printf("%i is negative\n", n);
 
 		return (0);
 
