e41ac6d68d6d60d08819adb27008b2f44d0e1c5c
festusmaithyakcau <festusmaithya264@gmail.com>
2022-12-14 01:19:37 -0800

    printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,6 @@
  *
  * Description:program that prints _putchar
  *
- *
  * Return : Always 0 (Success)
 */
 
@@ -14,7 +13,7 @@ int main(void)
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch=0; ch<8; ++ch)
+	for (ch = 0; ch < 8; ++ch)
 		_putchar(str[ch]);
 	_putchar('\n');
 
