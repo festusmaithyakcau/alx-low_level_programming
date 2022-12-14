13e0f815c9da87b790d0afc440f91bd708acd903
festusmaithyakcau <festusmaithya264@gmail.com>
2022-12-14 01:27:45 -0800

    printing lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
+#include"main.h"
 
+/*
+ * main-Entry point
+ *
+ * Description:printing alphabets in lowercase
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
