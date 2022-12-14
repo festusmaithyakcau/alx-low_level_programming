ff16e3a7a49cc8e24f9e9a0878f590ac70627168
festusmaithyakcau <festusmaithya264@gmail.com>
2022-12-14 01:12:35 -0800

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/*
+ * main-Entry point
+ *
+ * Description:program that prints _putchar
+ *
+ *
+ * Return : Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch=0; ch<8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
