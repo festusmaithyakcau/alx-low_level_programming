98bfa17c3ccb7eee39f1d2499eacfd4f9c37d168
festusmaithyakcau <festusmaithya264@gmail.com>
2022-12-22 02:54:37 -0800

    String concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+/**
+ * _strcat-function that concatenates strings
+ *
+ * @dest: pointer to destination pointer
+ * @src: pointer to the source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+char *_strcst(char *dest, char *src)
+{
+	int c, c2;
+
+	c = 0;
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
+}
