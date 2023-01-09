3c8f8fb3489a30c97cbd702e7f69866cdfc4ca30
festusmaithyakcau <festusmaithya264@gmail.com>
2023-01-09 03:53:30 -0800

    added a putchar file

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
