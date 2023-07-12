752677e77d6531b2410bb22b22553a11a0eddfea
Festus Maithya <festusmaithya264@gmail.com>
2023-07-12 20:20:40 +0300

    Update search_algos.h
    Updated the function prototype definitions

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,7 +13,7 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for ALX project
  */
 typedef struct listint_s
 {
@@ -31,7 +31,7 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
+ * for ALX project
  */
 typedef struct skiplist_s
 {
